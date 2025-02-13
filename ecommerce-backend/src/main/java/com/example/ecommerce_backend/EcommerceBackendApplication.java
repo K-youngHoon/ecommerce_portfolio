a54074c85a3e3f2f2e0c86d7160c5ca6a40ba39e
ROOTXBOT2 <rootxbot2@gmail.com>
2025-02-14 06:56:07 +0900

    Re:Initial Spring Boot project

--- ecommerce-backend/src/main/java/com/example/ecommerce_backend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/example/ecommerce_backend/EcommerceBackendApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerceweb.ecommerce_backend;
+package com.example.ecommerce_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
