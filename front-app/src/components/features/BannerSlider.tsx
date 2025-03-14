812f0fef49a2c0830849bafd2d02af98676782a7
kimyounghoon <kyhsos1119@fgb.ai>
2025-03-15 00:04:40 +0900

    test swiper

--- front-app/src/components/features/BannerSlider.tsx
+++ front-app/src/components/features/BannerSlider.tsx
@@ -6,95 +6,39 @@ import {
   Autoplay,
   Scrollbar,
   Thumbs,
+  FreeMode,
 } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import "swiper/css/scrollbar";
+// import "swiper/css/free-mode";
+// import "swiper/css/thumbs";
+// import "swiper/css/effect-fade";
+// import "./bannerSlider.scss";
+import "swiper/css";
+import "swiper/css/pagination";
 import styles from "./bannerSlider.module.scss";
-import slideStyles from "@src/styles/Slide.module.scss";
 
 // 배너 데이터
-const banners = [
-  {
-    id: 1,
-    image: "/images/banner1.jpg",
-    text: "네이버에서만 역대급 혜택",
-    date: "3.4 - 3.16",
-  },
-  {
-    id: 2,
-    image: "/images/banner2.jpg",
-    text: "선착순 10% 할인 쿠폰!",
-    subText: "러닝페스타 최대 70% 할인",
-  },
-  {
-    id: 3,
-    image: "/images/banner3.jpg",
-    text: "니베아 롤온 데오드란트",
-    subText: "72시간 지속 효과",
-  },
-];
 
 export const BannerSlider = () => {
   return (
-    <div className={styles.bannerContainer}>
-      {/* <Swiper
-        modules={[Navigation, Pagination, EffectFade]}
-        effect="fade"
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true, el: `.${styles.pagination}` }}
-        loop
-      >
-        {banners.map((banner) => (
-          <SwiperSlide key={banner.id}>
-            <div
-              className={styles.bannerSlide}
-              style={
-                {
-                  // backgroundImage: `url(${banner.image})`,
-                  // backgroundSize: "cover",
-                }
-              }
-            >
-              <div>
-                <h2>{banner.text}</h2>
-                {banner.subText && <p>{banner.subText}</p>}
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-        <div className={styles.pagination}></div>
-      </Swiper> */}
-      <div className={slideStyles.swiperContainer}>
-        {/* 메인 슬라이드 */}
-        <Swiper
-          modules={[
-            Navigation,
-            Pagination,
-            // Autoplay,
-            Scrollbar,
-            Thumbs,
-            EffectFade,
-          ]}
-          effect="fade" // 페이드 효과
-          spaceBetween={20}
-          slidesPerView={1}
-          navigation
-          pagination={{
-            clickable: true,
-            el: `.${slideStyles.customPagination}`,
-          }}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-          loop
-        >
-          {banners.map((banner, index) => (
-            <SwiperSlide key={index}>
-              <div className={`${slideStyles.slide}`}>{banner.text}</div>
-              <label>{banner.subText}</label>
-            </SwiperSlide>
-          ))}
-          <div className={slideStyles.customPagination}></div>{" "}
-          {/* 페이지네이션 */}
-        </Swiper>
-      </div>
-    </div>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className={styles.swiper}
+      loop
+    >
+      {Array.from({ length: 10 }, (_, i) => (
+        <SwiperSlide key={i} className={styles.swiperSlide}>
+          Slide {i + 1}
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
--- front-app/src/pages/index.tsx
+++ front-app/src/pages/index.tsx
@@ -1,22 +1,6 @@
 import { authHoc } from "@src/components/common";
 import { HomeHeader, HomeFooter } from "@src/components/layouts";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-  Autoplay,
-  Scrollbar,
-  Thumbs,
-  EffectFade,
-} from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/thumbs";
-import "swiper/css/effect-fade";
 import homeStyles from "./home.module.scss";
 import { BannerSlider } from "@src/components/features";
 
@@ -24,7 +8,7 @@ function Home() {
   return (
     <div>
       <HomeHeader />
-      <main className={homeStyles["main-container"]}>
+      <main className={homeStyles.mainContainer}>
         <BannerSlider />
       </main>
       <HomeFooter />
